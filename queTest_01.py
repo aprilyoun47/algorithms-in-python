dfff71e43b9cd8d90d3fffac0dfaf349c23a9886
april y <84491887+aprilyoun47@users.noreply.github.com>
2023-01-27 19:10:20 +0900

    Update queTest_01.py

--- queTest_01.py
+++ queTest_01.py
@@ -1,3 +1,11 @@
+"""
+주식 가격(QUE)
+https://school.programmers.co.kr/learn/courses/30/lessons/42584
+- 이중 for문으로 -> que를 사용해보자
+- 몇 분 동안 주식 가격이 하락하지 않는지 for문
+해당 금액에서 전체 list를 돌면서 가격 비교를 해야함. 해당 금액부터 start 이므로 
+"""
+
 from collections import deque
 
 def solution(prices):
